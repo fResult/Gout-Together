64fe656beaa7e9e8a424377d2fd64b8e90865162
fResult <styxmaz@gmail.com>
2024-11-13 17:00:03 +0700

    prepare `flyway` db migration configuration

--- src/main/java/dev/fResult/goutTogether/GoutTogetherApplication.java
+++ src/main/java/dev/fResult/goutTogether/GoutTogetherApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoutTogetherApplication {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         SpringApplication.run(GoutTogetherApplication.class, args);
     }
 }
