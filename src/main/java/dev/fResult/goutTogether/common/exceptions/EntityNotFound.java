693d5c0bc8a22062d01d009c651f0e97cf8ac5f3
fResult <styxmaz@gmail.com>
2024-11-15 17:32:59 +0700

    create `EntityNotFound` as an Exception class

---
+++ src/main/java/dev/fResult/goutTogether/common/exceptions/EntityNotFound.java
@@ -0,0 +1,11 @@
+package dev.fResult.goutTogether.common.exceptions;
+
+public class EntityNotFound extends RuntimeException {
+    public EntityNotFound() {
+        super("Entity not found");
+    }
+
+    public EntityNotFound(String message) {
+        super(message);
+    }
+}
