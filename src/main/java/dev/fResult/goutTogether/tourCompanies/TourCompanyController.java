8f7401d35f67bac9c061350b0520f58f81653bb9
fResult <styxmaz@gmail.com>
2024-11-15 16:40:20 +0700

    add `TourCompanyController.register` endpoint

--- src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
+++ src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
@@ -1,9 +1,16 @@
 package dev.fResult.goutTogether.tourCompanies;
 
+import dev.fResult.goutTogether.tourCompanies.models.TourCompany;
+import dev.fResult.goutTogether.tourCompanies.models.TourCompanyRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/tour-companies")
@@ -14,5 +21,12 @@ public class TourCompanyController {
 
     public TourCompanyController(TourCompanyServiceImpl tourCompanyService) {
         this.tourCompanyService = tourCompanyService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TourCompany> register(@RequestBody TourCompanyRequest body) {
+        logger.info("Registering a new tour company");
+        return ResponseEntity.created(URI.create("/api/v1/tour-companies"))
+                .body(tourCompanyService.registerTour(body));
     }
 }
