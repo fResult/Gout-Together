d98d8552509fdd3ebbc97d881c19f35e6a0d5711
fResult <styxmaz@gmail.com>
2024-11-15 15:46:34 +0700

    prepare `registerTourCompany` method

--- src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyRepository.java
+++ src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyRepository.java
@@ -1,5 +1,6 @@
 package dev.fResult.goutTogether.tourCompanies;
 
+import dev.fResult.goutTogether.tourCompanies.models.TourCompany;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TourCompanyRepository extends CrudRepository<TourCompany, Integer> {
