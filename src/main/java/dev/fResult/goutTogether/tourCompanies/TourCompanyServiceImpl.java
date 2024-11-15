35e5897546fcd427c1f000c894b7d24b45678c15
fResult <styxmaz@gmail.com>
2024-11-15 17:33:54 +0700

    implement `approveTourCompany` method in the `TourCompanyService`

--- src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyServiceImpl.java
+++ src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyServiceImpl.java
@@ -1,5 +1,6 @@
 package dev.fResult.goutTogether.tourCompanies;
 
+import dev.fResult.goutTogether.common.exceptions.EntityNotFound;
 import dev.fResult.goutTogether.tourCompanies.models.TourCompany;
 import dev.fResult.goutTogether.tourCompanies.models.TourCompanyRequest;
 import dev.fResult.goutTogether.tours.TourCompanyStatus;
@@ -7,10 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+
 @Service
 public class TourCompanyServiceImpl implements TourCompanyService {
     private final Logger logger = LoggerFactory.getLogger(TourCompanyServiceImpl.class);
-
     private final TourCompanyRepository tourCompanyRepository;
 
     public TourCompanyServiceImpl(TourCompanyRepository tourCompanyRepository) {
@@ -25,5 +27,19 @@ public class TourCompanyServiceImpl implements TourCompanyService {
         logger.info("[registerTour] new tour company: {}", registeredCompany);
 
         return registeredCompany;
+    }
+
+    @Override
+    public TourCompany approveTourCompany(int id) {
+        logger.debug("[approveTour] tour company with id: {} is approving", id);
+        return tourCompanyRepository.findById(id).map(existingCompany -> {
+            var companyToApprove = TourCompany.of(existingCompany.id(), existingCompany.name(), TourCompanyStatus.APPROVED.name());
+            var approvedCompany = tourCompanyRepository.save(companyToApprove);
+            logger.info("[approveTour] approved tour company: {}", approvedCompany);
+            return approvedCompany;
+        }).orElseThrow(() -> {
+            logger.warn("[approveTour] tour company with id: {} not found", id);
+            return new EntityNotFound(String.format("Tour company id: %s not found", id));
+        });
     }
 }
