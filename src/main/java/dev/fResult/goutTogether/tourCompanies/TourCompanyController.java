6fa1d11ea2b3af7556cac7b4d5793b137b4105f8
fResult <styxmaz@gmail.com>
2024-11-16 12:01:08 +0700

    create `handleMethodArgumentNotValid` in controller advice to override bad request response

--- src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
+++ src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
@@ -5,6 +5,7 @@ import dev.fResult.goutTogether.tourCompanies.models.TourCompanyRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -20,7 +21,7 @@ public class TourCompanyController {
     }
 
     @PostMapping
-    public ResponseEntity<TourCompany> register(@RequestBody TourCompanyRequest body) {
+    public ResponseEntity<TourCompany> register(@RequestBody @Validated TourCompanyRequest body) {
         logger.info("Registering a new tour company");
         return ResponseEntity.created(URI.create("/api/v1/tour-companies"))
                 .body(tourCompanyService.registerTourCompany(body));
