cb49cd58228d7f9718e9d5c2362e0664ef551fcc
fResult <styxmaz@gmail.com>
2024-11-16 12:01:08 +0700

    create `handleMethodArgumentNotValid` in controller advice to override bad request response

--- src/main/java/dev/fResult/goutTogether/common/ResponseAdviceHandler.java
+++ src/main/java/dev/fResult/goutTogether/common/ResponseAdviceHandler.java
@@ -1,17 +1,41 @@
 package dev.fResult.goutTogether.common;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import dev.fResult.goutTogether.common.exceptions.EntityNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
 
 @RestControllerAdvice
 public class ResponseAdviceHandler extends ResponseEntityExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseAdviceHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request) {
+        var propertyToError = new HashMap<String, Object>();
+        var detail = ProblemDetail.forStatusAndDetail(status, "Invalid request arguments");
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            propertyToError.put(
+                    error.getField(),
+                    error.getDefaultMessage());
+        });
+        detail.setProperty("arguments", propertyToError);
+
+        return ResponseEntity.of(detail).build();
+    }
+
     @ExceptionHandler(EntityNotFound.class)
     protected ResponseEntity<?> handleEntityNotFoundException(EntityNotFound ex) {
         var detail = ProblemDetail.forStatusAndDetail(
