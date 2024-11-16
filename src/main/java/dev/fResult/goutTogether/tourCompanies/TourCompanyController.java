20ae805aacb0bd1423fd97763e017be7b4e2b7a1
fResult <styxmaz@gmail.com>
2024-11-16 11:48:30 +0700

    add `handleEntityNotFoundException` in controller advice

--- src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
+++ src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
@@ -4,8 +4,6 @@ import dev.fResult.goutTogether.tourCompanies.models.TourCompany;
 import dev.fResult.goutTogether.tourCompanies.models.TourCompanyRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,15 +28,9 @@ public class TourCompanyController {
 
     @PostMapping("/{id}/approve")
     public ResponseEntity<TourCompany> approve(@PathVariable int id) {
-        logger.info("Approving a new tour company");
-        try {
-            logger.info("Approving a new tour company with id: {}", id);
-            var approvedCompany = tourCompanyService.approveTourCompany(id);
-            logger.info("[approvedCompany] company id: {} is approved", id);
-            return ResponseEntity.ok(approvedCompany);
-        } catch (Exception e) {
-            var detail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-            return ResponseEntity.of(detail).build();
-        }
+        logger.info("Approving a new tour company id [{}]", id);
+        var approvedCompany = tourCompanyService.approveTourCompany(id);
+        logger.info("[approvedCompany] company id [{}] is approved", id);
+        return ResponseEntity.ok(approvedCompany);
     }
 }
