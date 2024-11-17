0e4ed174b4b320b48d8f2498132e856f4fe9cfe6
fResult <styxmaz@gmail.com>
2024-11-17 21:17:13 +0700

    create `TourCountRepository` interface

---
+++ src/main/java/dev/fResult/goutTogether/tours/repositories/TourCountRepository.java
@@ -0,0 +1,6 @@
+package dev.fResult.goutTogether.tours.repositories;
+
+import dev.fResult.goutTogether.tours.entities.TourCount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourCountRepository extends CrudRepository<TourCount, Integer> {}
