fb6595a6381a47c0f839be8b5327a925e5068b75
fResult <styxmaz@gmail.com>
2024-11-19 02:23:38 +0700

    add `TourCompanyServiceTest.whenRegisterCompanyThenSuccess` test case

--- src/test/java/dev/fResult/goutTogether/tourCompanies/TourCompanyServiceTest.java
+++ src/test/java/dev/fResult/goutTogether/tourCompanies/TourCompanyServiceTest.java
@@ -1,4 +1,59 @@
 package dev.fResult.goutTogether.tourCompanies;
 
-public class TourCompanyServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import dev.fResult.goutTogether.common.enumurations.TourCompanyStatus;
+import dev.fResult.goutTogether.tourCompanies.dtos.RegisterTourCompanyRequest;
+import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
+import dev.fResult.goutTogether.tourCompanies.entities.TourCompanyLogin;
+import dev.fResult.goutTogether.tourCompanies.repositories.TourCompanyLoginRepository;
+import dev.fResult.goutTogether.tourCompanies.repositories.TourCompanyRepository;
+import dev.fResult.goutTogether.tourCompanies.repositories.TourCompanyWalletRepository;
+import dev.fResult.goutTogether.tourCompanies.services.TourCompanyServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class TourCompanyServiceTest {
+  @InjectMocks private TourCompanyServiceImpl tourCompanyService;
+
+  @Mock private TourCompanyRepository tourCompanyRepository;
+  @Mock private TourCompanyLoginRepository tourCompanyLoginRepository;
+  @Mock private TourCompanyWalletRepository tourCompanyWalletRepository;
+  @Mock private PasswordEncoder passwordEncoder;
+
+  @Test
+  void whenRegisterCompanyThenSuccess() {
+    var body = RegisterTourCompanyRequest.of(null, "My Tour", "MyTour", "mypassword", null);
+    var expectedRegisteredCompany = TourCompany.of(1, "My Tour", TourCompanyStatus.WAITING.name());
+
+    // Arrange
+    var mockTourCompany = TourCompany.of(1, "My Tour", TourCompanyStatus.WAITING.name());
+    var mockCompanyCredential =
+        TourCompanyLogin.of(
+            null,
+            AggregateReference.to(mockTourCompany.id()),
+            body.username(),
+            "encryptedPassword");
+    when(tourCompanyRepository.save(any(TourCompany.class))).thenReturn(mockTourCompany);
+    when(passwordEncoder.encode(Mockito.anyString())).thenReturn("encryptedPassword");
+    when(tourCompanyLoginRepository.save(any(TourCompanyLogin.class)))
+        .thenReturn(mockCompanyCredential);
+
+    // Actual
+    var actualRegisteredCompany = tourCompanyService.registerTourCompany(body);
+
+    // Assert
+    assertNotNull(actualRegisteredCompany);
+    assertEquals(expectedRegisteredCompany, actualRegisteredCompany);
+  }
 }
