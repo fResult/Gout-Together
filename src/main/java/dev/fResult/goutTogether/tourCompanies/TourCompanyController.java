f36ff452a3d8ef90a5939a56ee64016d6c7de3a2
fResult <styxmaz@gmail.com>
2024-11-19 02:18:31 +0700

    update the constructor injection type for `TourCompanyController.tourCompanyService`

--- src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
+++ src/main/java/dev/fResult/goutTogether/tourCompanies/TourCompanyController.java
@@ -1,23 +1,22 @@
 package dev.fResult.goutTogether.tourCompanies;
 
-import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
 import dev.fResult.goutTogether.tourCompanies.dtos.RegisterTourCompanyRequest;
-import dev.fResult.goutTogether.tourCompanies.services.TourCompanyServiceImpl;
+import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
+import dev.fResult.goutTogether.tourCompanies.services.TourCompanyService;
+import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/api/v1/tour-companies")
 public class TourCompanyController {
   private final Logger logger = LoggerFactory.getLogger(TourCompanyController.class);
-  private final TourCompanyServiceImpl tourCompanyService;
+  private final TourCompanyService tourCompanyService;
 
-  public TourCompanyController(TourCompanyServiceImpl tourCompanyService) {
+  public TourCompanyController(TourCompanyService tourCompanyService) {
     this.tourCompanyService = tourCompanyService;
   }
 
