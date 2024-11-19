07f795a8a304fc244f220db927d7e6af30a520ab
fResult <styxmaz@gmail.com>
2024-11-19 14:02:10 +0700

    prepare `TourServiceTest` test class

--- src/test/java/dev/fResult/goutTogether/tours/TourServiceTest.java
+++ src/test/java/dev/fResult/goutTogether/tours/TourServiceTest.java
@@ -1,4 +1,19 @@
 package dev.fResult.goutTogether.tours;
 
-public class TourServiceTest {
+import dev.fResult.goutTogether.tourCompanies.services.TourCompanyService;
+import dev.fResult.goutTogether.tours.repositories.TourRepository;
+import dev.fResult.goutTogether.tours.services.TourCountService;
+import dev.fResult.goutTogether.tours.services.TourServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TourServiceTest {
+  @InjectMocks private TourServiceImpl tourService;
+
+  @Mock private TourRepository tourRepository;
+  @Mock private TourCompanyService tourCompanyService;
+  @Mock private TourCountService tourCountService;
 }
