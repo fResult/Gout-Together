c4d20b8a3cac9c11e55ce1f9364bc91ec2c26e5a
fResult <styxmaz@gmail.com>
2024-11-20 01:13:18 +0700

    create `TourControllerTest.whenCreateTourThenSuccess` test case

--- src/test/java/dev/fResult/goutTogether/tours/TourControllerTest.java
+++ src/test/java/dev/fResult/goutTogether/tours/TourControllerTest.java
@@ -1,24 +1,35 @@
 package dev.fResult.goutTogether.tours;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.fResult.goutTogether.common.enumurations.TourStatus;
+import dev.fResult.goutTogether.common.exceptions.EntityNotFound;
+import dev.fResult.goutTogether.tours.dtos.TourRequest;
 import dev.fResult.goutTogether.tours.entities.Tour;
 import dev.fResult.goutTogether.tours.services.TourService;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebMvcTest(TourController.class)
@@ -26,6 +37,7 @@ class TourControllerTest {
   private static final String TOUR_API = "/api/v1/tours";
 
   @Autowired private WebApplicationContext webApplicationContext;
+  @Autowired private ObjectMapper objectMapper;
 
   @MockBean private TourService tourService;
 
@@ -57,5 +69,45 @@ class TourControllerTest {
 
     // Assert
     resultActions.andExpect(status().isOk()).andExpect(jsonPath("$.id").value(TOUR_ID));
+  }
+  @Test
+  void whenCreateTourThenSuccess() throws Exception {
+    // Arrange
+    var TOUR_ID = 1;
+    var TOUR_COMPANY_ID = 1;
+    var body =
+        TourRequest.of(
+            TOUR_COMPANY_ID,
+            "Kunlun 7 days",
+            "Go 12 places around Kunlun",
+            "Kunlun, China",
+            20,
+            Instant.now().plus(Duration.ofDays(45)),
+            null);
+    var mockTour =
+        Tour.of(
+            TOUR_ID,
+            AggregateReference.to(body.tourCompanyId()),
+            body.title(),
+            body.description(),
+            body.location(),
+            body.numberOfPeople(),
+            body.activityDate(),
+            TourStatus.PENDING.name());
+    when(tourService.createTour(any(TourRequest.class))).thenReturn(mockTour);
+
+    // Actual
+    var actualResults =
+        mockMvc.perform(
+            post(TOUR_API)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(body)));
+
+    // Assert
+    actualResults
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").value(TOUR_ID))
+        .andExpect(jsonPath("$.tourCompanyId.id").value(TOUR_COMPANY_ID))
+        .andExpect(jsonPath("$.status").value(TourStatus.PENDING.name()));
   }
 }
