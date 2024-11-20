ca86c9953afe213de1b194fcc66e2c33dcbad10f
fResult <styxmaz@gmail.com>
2024-11-20 16:33:07 +0700

    prepare `UserLoginRepository`

---
+++ src/main/java/dev/fResult/goutTogether/users/repositories/UserLoginRepository.java
@@ -0,0 +1,6 @@
+package dev.fResult.goutTogether.users.repositories;
+
+import dev.fResult.goutTogether.auths.UserLogin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserLoginRepository extends CrudRepository<UserLogin, Integer> {}
