d1d6ae36cfef86a2435c25ef31b293146542bd97
fResult <styxmaz@gmail.com>
2024-11-22 17:29:32 +0700

    enhance AggregateReferenceToUpdateBuilder class to throw exception if data is null

--- src/main/java/dev/fResult/goutTogether/helpers/AggregateReferenceToUpdateBuilder.java
+++ src/main/java/dev/fResult/goutTogether/helpers/AggregateReferenceToUpdateBuilder.java
@@ -4,11 +4,19 @@ import java.util.Objects;
 import java.util.Optional;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
+import static org.springframework.util.StringUtils.capitalize;
+
 public class AggregateReferenceToUpdateBuilder {
   public static <T, ID> AggregateReference<T, ID> build(
-      ID idFromRequest, AggregateReference<T, ID> aggregateRefIdFromDb) {
+      ID idFromRequest, AggregateReference<T, ID> aggregateRefIdFromDb, String resourceName) {
+    var existingId =
+        Optional.ofNullable(aggregateRefIdFromDb.getId())
+            .orElseThrow(
+                () ->
+                    new RuntimeException(String.format("%s id is null", capitalize(resourceName))));
+
     return Optional.ofNullable(idFromRequest)
         .map(AggregateReference::<T, ID>to)
-        .orElse(AggregateReference.to(Objects.requireNonNull(aggregateRefIdFromDb.getId())));
+        .orElse(AggregateReference.to(existingId));
   }
 }
