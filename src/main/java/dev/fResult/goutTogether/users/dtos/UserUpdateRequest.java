a7271a007f2edda786ee326d23fec3a38b24d5a0
fResult <styxmaz@gmail.com>
2024-11-24 14:58:53 +0700

    Create `UserUpdateRequest` record class as a DTO.

--- src/main/java/dev/fResult/goutTogether/users/dtos/UserUpdateRequest.java
+++ src/main/java/dev/fResult/goutTogether/users/dtos/UserUpdateRequest.java
@@ -1,7 +1,20 @@
 package dev.fResult.goutTogether.users.dtos;
 
+import dev.fResult.goutTogether.users.entities.User;
+import java.util.Optional;
+import java.util.function.Function;
+
 public record UserUpdateRequest(String firstName, String lastName, String phoneNumber) {
   public static UserUpdateRequest of(String firstName, String lastName, String phoneNumber) {
     return new UserUpdateRequest(firstName, lastName, phoneNumber);
+  }
+
+  public static Function<User, User> dtoToUserUpdate(UserUpdateRequest body) {
+    return user ->
+        User.of(
+            user.id(),
+            Optional.ofNullable(body.firstName()).orElse(user.firstName()),
+            Optional.ofNullable(body.lastName()).orElse(user.lastName()),
+            Optional.ofNullable(body.phoneNumber()).orElse(user.phoneNumber()));
   }
 }
