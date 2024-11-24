fe58168572f3e698093fc167a96ab63af4b11512
fResult <styxmaz@gmail.com>
2024-11-24 17:03:00 +0700

    Update Flyway migrations.
    - Add ON DELETE CASCADE at the reference of `user_logins.user_id`
    - Add ON DELETE CASCADE at the reference of `user_points.user_id`
    - Add ON DELETE CASCADE at the reference of `user_roles.user_id` and `user_roles.role_id`
    - Add ON DELETE CASCADE at the reference of `user_wallets.user_id`
    - Add condition to prevent updating `roles_id_seq` if role seq is not over current roles' length

--- src/main/resources/db/migration/V8__insert_required_role_to_roles_table.sql
+++ src/main/resources/db/migration/V8__insert_required_role_to_roles_table.sql
@@ -1,3 +1,9 @@
-INSERT INTO roles (id, name)
-VALUES (1, 'ADMIN'), (2, 'CONSUMER'), (3, 'COMPANY');
-SELECT setval('roles_id_seq', 4, false);
+DO $$
+BEGIN
+    INSERT INTO roles (id, name)
+    VALUES (1, 'ADMIN'), (2, 'CONSUMER'), (3, 'COMPANY');
+
+    IF (SELECT last_value FROM roles_id_seq) < 4 THEN
+        PERFORM setval('roles_id_seq', 4, false);
+    END IF;
+END $$;
