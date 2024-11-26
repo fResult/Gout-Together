9e249cf91869697893fc4b348dc5adeef4ecce9e
fResult <styxmaz@gmail.com>
2024-11-26 17:43:10 +0700

    Fix bug in `createTourCompanyWallet` after creating its unit test case

--- src/main/java/dev/fResult/goutTogether/wallets/services/WalletServiceImpl.java
+++ src/main/java/dev/fResult/goutTogether/wallets/services/WalletServiceImpl.java
@@ -1,7 +1,6 @@
 package dev.fResult.goutTogether.wallets.services;
 
 import dev.fResult.goutTogether.helpers.ErrorHelper;
-import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
 import dev.fResult.goutTogether.wallets.entities.TourCompanyWallet;
 import dev.fResult.goutTogether.wallets.entities.UserWallet;
 import dev.fResult.goutTogether.wallets.repositories.TourCompanyWalletRepository;
@@ -62,12 +61,11 @@ public class WalletServiceImpl implements WalletService {
     logger.debug(
         "[createTourCompanyWallet] New {} is creating", TourCompanyWallet.class.getSimpleName());
 
-    AggregateReference<TourCompany, Integer> tourCompanyReference =
-        AggregateReference.to(tourCompanyId);
-    var createdWallet =
-        TourCompanyWallet.of(null, tourCompanyReference, Instant.now(), BigDecimal.ZERO);
+    var walletToCreate =
+        TourCompanyWallet.of(
+            null, AggregateReference.to(tourCompanyId), Instant.now(), BigDecimal.ZERO);
 
-    tourCompanyWalletRepository.save(createdWallet);
+    var createdWallet = tourCompanyWalletRepository.save(walletToCreate);
     logger.info(
         "[createTourCompanyWallet] New {} is created: {}",
         TourCompanyWallet.class.getSimpleName(),
