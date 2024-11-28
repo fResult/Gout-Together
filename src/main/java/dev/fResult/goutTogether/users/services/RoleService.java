6413aef321adfad737c0e13cd6ff7aa8422ffdeb
fResult <styxmaz@gmail.com>
2024-11-29 04:38:15 +0700

    Create `RoleService.bindUser` method

--- src/main/java/dev/fResult/goutTogether/users/services/RoleService.java
+++ src/main/java/dev/fResult/goutTogether/users/services/RoleService.java
@@ -2,19 +2,27 @@ package dev.fResult.goutTogether.users.services;
 
 import java.util.List;
 
+import dev.fResult.goutTogether.common.enumurations.UserRoleName;
 import dev.fResult.goutTogether.users.entities.Role;
+import dev.fResult.goutTogether.users.entities.User;
+import dev.fResult.goutTogether.users.entities.UserRole;
 import dev.fResult.goutTogether.users.repositories.RoleRepository;
+import dev.fResult.goutTogether.users.repositories.UserRoleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService {
   private final Logger logger = LoggerFactory.getLogger(RoleService.class);
-  private final RoleRepository roleRepository;
 
-  public RoleService(RoleRepository roleRepository) {
+  private final RoleRepository roleRepository;
+  private final UserRoleRepository userRoleRepository;
+
+  public RoleService(RoleRepository roleRepository, UserRoleRepository userRoleRepository) {
     this.roleRepository = roleRepository;
+    this.userRoleRepository = userRoleRepository;
   }
 
   public List<Role> getRoles() {
@@ -22,5 +30,17 @@ public class RoleService {
     logger.info("[getRoles] Available Roles: {}", availableRoles);
 
     return availableRoles;
+  }
+
+  public UserRole bindNewUser(int userId, UserRoleName roleName) {
+    logger.debug("[bindNewUser] new {} is binding", UserRole.class.getSimpleName());
+    var userRoleToBind =
+        UserRole.of(null, AggregateReference.to(userId), AggregateReference.to(roleName.getId()));
+
+    var boundUserRole = userRoleRepository.save(userRoleToBind);
+    logger.info(
+        "[bindNewUser] New {} is registered: {}", UserRole.class.getSimpleName(), boundUserRole);
+
+    return boundUserRole;
   }
 }
