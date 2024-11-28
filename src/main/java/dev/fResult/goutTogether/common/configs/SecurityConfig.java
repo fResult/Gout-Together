c6bff5aa7d24dda43f942fc926b410155853cf54
fResult <styxmaz@gmail.com>
2024-11-29 04:42:44 +0700

    Config `JwtEncoder` and `JwtDecoder` bean in `SecurityConfig`.

--- src/main/java/dev/fResult/goutTogether/common/configs/SecurityConfig.java
+++ src/main/java/dev/fResult/goutTogether/common/configs/SecurityConfig.java
@@ -1,18 +1,44 @@
 package dev.fResult.goutTogether.common.configs;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.proc.SecurityContext;
+import dev.fResult.goutTogether.common.enumurations.UserRoleName;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  private final ResourceLoader resourceLoader;
+
+  public SecurityConfig(ResourceLoader resourceLoader) {
+    this.resourceLoader = resourceLoader;
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
@@ -39,4 +65,51 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
   }
+
+  @Bean
+  public JwtEncoder jwtEncoder(RSAKeyProperties rsaInstance) {
+    var jwk =
+        new RSAKey.Builder(rsaInstance.publicKey).privateKey(rsaInstance.privateKey).build();
+    var jwkSource = new ImmutableJWKSet<SecurityContext>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwkSource);
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder(RSAKeyProperties rsaInstance) {
+    return NimbusJwtDecoder.withPublicKey(rsaInstance.publicKey).build();
+  }
+
+  @Bean
+  public RSAKeyProperties rsaInstance()
+      throws InvalidKeySpecException, IOException, NoSuchAlgorithmException, URISyntaxException {
+
+    var privateKeyPkcs8Resource = resourceLoader.getResource("classPath:private_key_pkcs8.pem");
+    var publicKeyResource = resourceLoader.getResource("classPath:public_key.pem");
+
+    var privateKeyContent = new String(privateKeyPkcs8Resource.getContentAsByteArray());
+    var publicKeyContent = new String(publicKeyResource.getContentAsByteArray());
+
+    privateKeyContent =
+        privateKeyContent
+            .replaceAll("\\n", "")
+            .replace("-----BEGIN PRIVATE KEY-----", "")
+            .replace("-----END PRIVATE KEY-----", "");
+    publicKeyContent =
+        publicKeyContent
+            .replaceAll("\\n", "")
+            .replace("-----BEGIN PUBLIC KEY-----", "")
+            .replace("-----END PUBLIC KEY-----", "");
+
+    var keyFactory = KeyFactory.getInstance("RSA");
+
+    var keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent));
+    var privateKey = (RSAPrivateKey) keyFactory.generatePrivate(keySpecPKCS8);
+
+    var keySpecX509 = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyContent));
+    var publicKey = (RSAPublicKey) keyFactory.generatePublic(keySpecX509);
+
+    return new RSAKeyProperties(publicKey, privateKey);
+  }
+
+  public static record RSAKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {}
 }
