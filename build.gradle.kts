05f8158b0de46980d61d64ae9e7534b58ad07411
fResult <styxmaz@gmail.com>
2024-11-29 03:15:32 +0700

    Install `org.springframework.boot:spring-boot-starter-oauth2-resource-server` package.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,13 @@ import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
+plugins {
+  application
+  java
+  id("org.springframework.boot") version "3.4.0"
+  id("io.spring.dependency-management") version "1.1.6"
+}
+
 buildscript {
   repositories {
     mavenCentral()
@@ -9,13 +16,6 @@ buildscript {
   dependencies {
     classpath("io.github.cdimascio:java-dotenv:5.2.2")
   }
-}
-
-plugins {
-  application
-  java
-  id("org.springframework.boot") version "3.4.0"
-  id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "dev.fResult"
@@ -40,10 +40,11 @@ dependencies {
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
   implementation("org.bouncycastle:bcprov-jdk18on:1.79") // As an Argon2PasswordEncoder's dependency in Spring Security
   implementation("org.flywaydb:flyway-core")
   implementation("org.flywaydb:flyway-database-postgresql")
