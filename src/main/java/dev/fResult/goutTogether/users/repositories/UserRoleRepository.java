9cfc9aea582eadab663e34725462faf33e457811
fResult <styxmaz@gmail.com>
2024-11-29 04:38:15 +0700

    Create `RoleService.bindUser` method

---
+++ src/main/java/dev/fResult/goutTogether/users/repositories/UserRoleRepository.java
@@ -0,0 +1,6 @@
+package dev.fResult.goutTogether.users.repositories;
+
+import dev.fResult.goutTogether.users.entities.UserRole;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface UserRoleRepository extends ListCrudRepository<UserRole, Integer> {}
