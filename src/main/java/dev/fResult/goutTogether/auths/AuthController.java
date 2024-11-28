73b3287fd1a6d1f59df775e8c77e244f6fb4b339
fResult <styxmaz@gmail.com>
2024-11-29 04:40:01 +0700

    Create `AuthController` and `AdminController` classes.

---
+++ src/main/java/dev/fResult/goutTogether/auths/AuthController.java
@@ -0,0 +1,10 @@
+package dev.fResult.goutTogether.auths;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auths")
+public class AuthController {
+
+}
