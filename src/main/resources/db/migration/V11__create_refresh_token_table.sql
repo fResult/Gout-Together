c9369af71e6de61a244122a4f6bf3cfc8e2c9b96
fResult <styxmaz@gmail.com>
2024-12-01 01:51:45 +0700

    Update the token column from VARCHAR(40) to VARCHAR(3000).

--- src/main/resources/db/migration/V11__create_refresh_token_table.sql
+++ src/main/resources/db/migration/V11__create_refresh_token_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS refresh_tokens (
     id SERIAL PRIMARY KEY,
-    token VARCHAR(40) NOT NULL,
+    token VARCHAR(3000) NOT NULL,
     issued_date TIMESTAMP WITH TIME ZONE NOT NULL,
     usage VARCHAR(7) NOT NULL,
     resource_id INTEGER NOT NULL,
