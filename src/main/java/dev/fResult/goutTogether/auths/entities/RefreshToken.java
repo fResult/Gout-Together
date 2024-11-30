cf368c4b860f7d480742ddbbd6bbb032128c0d50
fResult <styxmaz@gmail.com>
2024-11-30 21:51:16 +0700

    Create `RefreshToken` record class.

--- src/main/java/dev/fResult/goutTogether/auths/entities/RefreshToken.java
+++ src/main/java/dev/fResult/goutTogether/auths/entities/RefreshToken.java
@@ -1,4 +1,28 @@
 package dev.fResult.goutTogether.auths.entities;
 
-public record RefreshToken() {
+import dev.fResult.goutTogether.common.enumurations.UserRoleName;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.Instant;
+
+@Table("refresh_tokens")
+public record RefreshToken(
+    @Id Integer id,
+    String token,
+    Instant issuedDate,
+    UserRoleName usage,
+    int resourceId,
+    boolean isExpired) {
+
+  public RefreshToken of(
+      Integer id,
+      String token,
+      Instant issuedDate,
+      UserRoleName usage,
+      int resourceId,
+      boolean isExpired) {
+
+    return new RefreshToken(id, token, issuedDate, usage, resourceId, isExpired);
+  }
 }
