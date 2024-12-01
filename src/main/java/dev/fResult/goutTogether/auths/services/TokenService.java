7dd8bafc7713eb8122ffbf796a39f470c2d446d2
fResult <styxmaz@gmail.com>
2024-12-02 03:24:33 +0700

    Create methods in `TokenService`.
    - `issueAccessToken` overloaded method
    - `generateToken` overloaded method
    - `isRefreshTokenExpired` method

--- src/main/java/dev/fResult/goutTogether/auths/services/TokenService.java
+++ src/main/java/dev/fResult/goutTogether/auths/services/TokenService.java
@@ -4,6 +4,8 @@ import static dev.fResult.goutTogether.common.Constants.RESOURCE_ID_CLAIM;
 import static dev.fResult.goutTogether.common.Constants.ROLES_CLAIM;
 
 import dev.fResult.goutTogether.auths.dtos.AuthenticatedUser;
+import dev.fResult.goutTogether.auths.entities.RefreshToken;
+import dev.fResult.goutTogether.auths.entities.UserLogin;
 import dev.fResult.goutTogether.common.utils.UUIDV7;
 import java.time.Instant;
 import java.util.stream.Collectors;
@@ -19,14 +21,17 @@ import org.springframework.stereotype.Service;
 public class TokenService {
   private static final String ISSUER = "gout-together";
 
+  private final CustomUserDetailsService userDetailsService;
   private final long accessTokenExpiredInSeconds;
   private final long refreshTokenExpiredInSeconds;
   private final JwtEncoder jwtEncoder;
 
   public TokenService(
+      CustomUserDetailsService userDetailsService,
       @Value("${token.access-token-expired-in-seconds}") long accessTokenExpiredInSeconds,
       @Value("${token.refresh-token-expired-in-seconds}") long refreshTokenExpiredInSeconds,
       JwtEncoder jwtEncoder) {
+    this.userDetailsService = userDetailsService;
     this.accessTokenExpiredInSeconds = accessTokenExpiredInSeconds;
     this.refreshTokenExpiredInSeconds = refreshTokenExpiredInSeconds;
     this.jwtEncoder = jwtEncoder;
@@ -34,6 +39,13 @@ public class TokenService {
 
   public String issueAccessToken(Authentication authentication, Instant issuedAt) {
     return generateToken(authentication, issuedAt, accessTokenExpiredInSeconds);
+  }
+
+  public String issueAccessToken(UserLogin userLogin, Instant issuedAt) {
+    var authenticatedUser =
+        (AuthenticatedUser) userDetailsService.loadUserByUsername(userLogin.email());
+
+    return generateToken(authenticatedUser, issuedAt, accessTokenExpiredInSeconds);
   }
 
   public String issueRefreshToken() {
@@ -64,7 +76,35 @@ public class TokenService {
     return encodeClaimToJwt(claims);
   }
 
+  public String generateToken(
+      AuthenticatedUser authenticatedUser, Instant issuedAt, long expiredInSeconds) {
+    var scope =
+        authenticatedUser.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(" "));
+
+    var expiresAt = issuedAt.plusSeconds(expiredInSeconds);
+
+    var claims =
+        JwtClaimsSet.builder()
+            .issuer(ISSUER)
+            .issuedAt(issuedAt)
+            .subject(authenticatedUser.email())
+            .claim(ROLES_CLAIM, scope)
+            .claim(RESOURCE_ID_CLAIM, authenticatedUser.userId())
+            .expiresAt(expiresAt)
+            .build();
+
+    return encodeClaimToJwt(claims);
+  }
+
   public String encodeClaimToJwt(JwtClaimsSet claims) {
     return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+  }
+
+  public boolean isRefreshTokenExpired(RefreshToken refreshToken) {
+    var expiredAt = refreshToken.issuedDate().plusSeconds(refreshTokenExpiredInSeconds);
+
+    return Instant.now().isAfter(expiredAt);
   }
 }
