10201bb7224130931dc9c9355ef30235c3a93d49
fResult <styxmaz@gmail.com>
2024-12-02 03:19:31 +0700

    Rename constant variables.

--- src/main/java/dev/fResult/goutTogether/common/Constants.java
+++ src/main/java/dev/fResult/goutTogether/common/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
   private Constants() {}
 
   public static final String TOKEN_TYPE = "bearer";
-  public static final String ROLE_CLAIM = "roles";
+  public static final String ROLES_CLAIM = "roles";
   public static final String RESOURCE_ID_CLAIM = "resource_id";
 }
