4226f4a20eed55d248814ce5bd42d4328a9c2146
fResult <styxmaz@gmail.com>
2024-12-04 15:49:17 +0700

    Update POST `/auths/refresh` to support refresh token for Company side.

--- src/main/java/dev/fResult/goutTogether/auths/services/AuthService.java
+++ src/main/java/dev/fResult/goutTogether/auths/services/AuthService.java
@@ -1,10 +1,10 @@
 package dev.fResult.goutTogether.auths.services;
 
-import dev.fResult.goutTogether.auths.dtos.RefreshTokenRequest;
 import dev.fResult.goutTogether.auths.dtos.AuthenticatedUser;
 import dev.fResult.goutTogether.auths.dtos.LoginRequest;
 import dev.fResult.goutTogether.auths.dtos.LoginResponse;
 import dev.fResult.goutTogether.auths.dtos.LogoutInfo;
+import dev.fResult.goutTogether.auths.dtos.RefreshTokenRequest;
 import dev.fResult.goutTogether.auths.entities.TourCompanyLogin;
 import dev.fResult.goutTogether.auths.entities.UserLogin;
 import java.util.*;
