4f428aeaa3ee6d01f0d7b58792d04b81f4ac9d6d
fResult <styxmaz@gmail.com>
2024-12-04 15:33:04 +0700

    Prepare loadUserByUsername to support company authorization.

--- src/main/java/dev/fResult/goutTogether/auths/services/CustomUserDetailsService.java
+++ src/main/java/dev/fResult/goutTogether/auths/services/CustomUserDetailsService.java
@@ -1,13 +1,18 @@
 package dev.fResult.goutTogether.auths.services;
 
 import dev.fResult.goutTogether.auths.dtos.AuthenticatedUser;
+import dev.fResult.goutTogether.auths.entities.TourCompanyLogin;
 import dev.fResult.goutTogether.auths.entities.UserLogin;
 import dev.fResult.goutTogether.auths.repositories.UserLoginRepository;
 import dev.fResult.goutTogether.common.enumurations.UserRoleName;
 import dev.fResult.goutTogether.helpers.ErrorHelper;
+import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
+import dev.fResult.goutTogether.tourCompanies.repositories.TourCompanyLoginRepository;
+import dev.fResult.goutTogether.users.entities.User;
 import dev.fResult.goutTogether.users.repositories.UserRoleRepository;
 import java.util.Objects;
 import java.util.function.Predicate;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
@@ -24,12 +29,16 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   private final UserLoginRepository userLoginRepository;
   private final UserRoleRepository userRoleRepository;
+  private final TourCompanyLoginRepository tourCompanyLoginRepository;
 
   public CustomUserDetailsService(
-      UserLoginRepository userLoginRepository, UserRoleRepository userRoleRepository) {
+      UserLoginRepository userLoginRepository,
+      UserRoleRepository userRoleRepository,
+      TourCompanyLoginRepository tourCompanyLoginRepository) {
 
     this.userLoginRepository = userLoginRepository;
     this.userRoleRepository = userRoleRepository;
+    this.tourCompanyLoginRepository = tourCompanyLoginRepository;
   }
 
   @Override
@@ -38,12 +47,23 @@ public class CustomUserDetailsService implements UserDetailsService {
         "[loadUserByUsername] {} username {} is loading",
         UserLogin.class.getSimpleName(),
         username);
+
+    if (isEmail(username)) return loginUser(username);
+    return loginTourCompany(username);
+  }
+
+  private boolean isEmail(String username) {
+    return EmailValidator.getInstance().isValid(username);
+  }
+
+  private AuthenticatedUser loginUser(String email) {
+    logger.debug("[loginUser] {} is logging in", User.class.getSimpleName());
     var userLogin =
         userLoginRepository
-            .findOneByEmail(username)
+            .findOneByEmail(email)
             .orElseThrow(
                 errorHelper.entityWithSubResourceNotFound(
-                    "loadUserByUsername", UserLogin.class, "email", username));
+                    "loginUser", UserLogin.class, "email", email));
 
     var userId = Objects.requireNonNull(userLogin.userId().getId());
 
@@ -52,7 +72,7 @@ public class CustomUserDetailsService implements UserDetailsService {
             .findOneByUserId(AggregateReference.to(userId))
             .orElseThrow(
                 errorHelper.entityWithSubResourceNotFound(
-                    "loadUserByUsername", UserLogin.class, "userId", String.valueOf(userId)));
+                    "loginUser", UserLogin.class, "userId", String.valueOf(userId)));
 
     Predicate<Integer> isAdmin =
         roleId -> Objects.requireNonNull(roleId) == UserRoleName.ADMIN.getId();
@@ -60,5 +80,21 @@ public class CustomUserDetailsService implements UserDetailsService {
     var role = isAdmin.test(userRole.roleId().getId()) ? UserRoleName.ADMIN : UserRoleName.CONSUMER;
 
     return AuthenticatedUser.of(userId, userLogin.email(), userLogin.password(), role);
+  }
+
+  private AuthenticatedUser loginTourCompany(String username) {
+    logger.debug("[loginTourCompany] {} is logging in", TourCompany.class.getSimpleName());
+    var companyCredential =
+        tourCompanyLoginRepository
+            .findOneByUsername(username)
+            .orElseThrow(
+                errorHelper.entityWithSubResourceNotFound(
+                    "loginTourCompany", TourCompanyLogin.class, "username", username));
+
+    return AuthenticatedUser.of(
+        companyCredential.id(),
+        companyCredential.username(),
+        companyCredential.password(),
+        UserRoleName.COMPANY);
   }
 }
