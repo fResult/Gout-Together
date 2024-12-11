701ed38b5bebe5a203240ca73b8c46653af93955
fResult <styxmaz@gmail.com>
2024-12-11 23:00:21 +0700

    Config testcontainers bean.

--- src/test/java/dev/fResult/goutTogether/configs/TestContainerConfig.java
+++ src/test/java/dev/fResult/goutTogether/configs/TestContainerConfig.java
@@ -1,14 +1,20 @@
 package dev.fResult.goutTogether.configs;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfig {
-  // FIXME: Uncomment this code to enable PostgreSQLContainer, and find the way to start Docker using Colima
-  // @Bean
-  // public PostgreSQLContainer<?> postgreSQLContainer() {
-  //   var container = new PostgreSQLContainer<>("postgres:13.3");
-  //   container.start();
-  //   return container;
-  // }
+  // FIXME: Find the way to start Docker using Colima
+  @Bean
+  @ServiceConnection
+  public PostgreSQLContainer<?> postgreSQLContainer() {
+    System.setProperty(
+        "TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE", "/Users/fresult/.colima/default/docker.sock");
+    System.setProperty("DOCKER_HOST", "unix:///Users/korn/.colima/default/docker.sock");
+
+    return new PostgreSQLContainer<>("postgres:13.3");
+  }
 }
