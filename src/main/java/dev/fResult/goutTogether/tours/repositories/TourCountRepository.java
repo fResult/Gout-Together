5bd2e6d9dad50ec6b78c8366e5e96a1083a9ddec
fResult <styxmaz@gmail.com>
2024-12-14 01:43:24 +0700

    Try Pessimistic Lock with PESSIMISTIC_WRITE lock mode.

--- src/main/java/dev/fResult/goutTogether/tours/repositories/TourCountRepository.java
+++ src/main/java/dev/fResult/goutTogether/tours/repositories/TourCountRepository.java
@@ -1,6 +1,15 @@
 package dev.fResult.goutTogether.tours.repositories;
 
+import dev.fResult.goutTogether.tours.entities.Tour;
 import dev.fResult.goutTogether.tours.entities.TourCount;
+import java.util.Optional;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.sql.LockMode;
+import org.springframework.data.relational.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TourCountRepository extends CrudRepository<TourCount, Integer> {}
+public interface TourCountRepository extends CrudRepository<TourCount, Integer> {
+
+  @Lock(LockMode.PESSIMISTIC_WRITE)
+  Optional<TourCount> findOneByTourId(AggregateReference<Tour, Integer> tourId);
+}
