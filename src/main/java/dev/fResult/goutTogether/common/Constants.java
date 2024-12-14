751d54fa41473813ada4df49ad122d0e1d7a18c9
fResult <styxmaz@gmail.com>
2024-12-15 01:43:49 +0700

    Refactor, add new constant variables, add new repo's method.

--- src/main/java/dev/fResult/goutTogether/common/Constants.java
+++ src/main/java/dev/fResult/goutTogether/common/Constants.java
@@ -3,7 +3,9 @@ package dev.fResult.goutTogether.common;
 public class Constants {
   private Constants() {}
 
-  public static final String TOKEN_TYPE = "bearer";
   public static final String ROLES_CLAIM = "roles";
   public static final String RESOURCE_ID_CLAIM = "resource_id";
+
+  public static final String TOKEN_TYPE = "bearer";
+  public static final String API_PAYMENT_PATH = "/api/v1/payments";
 }
--- src/main/java/dev/fResult/goutTogether/qrcodes/QrCodeReferenceRepository.java
+++ src/main/java/dev/fResult/goutTogether/qrcodes/QrCodeReferenceRepository.java
@@ -1,5 +1,8 @@
 package dev.fResult.goutTogether.qrcodes;
 
+import java.util.Optional;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface QrCodeReferenceRepository extends ListCrudRepository<QrCodeReference, Integer> {}
+public interface QrCodeReferenceRepository extends ListCrudRepository<QrCodeReference, Integer> {
+  Optional<QrCodeReference> findOneByBookingId(int bookingId);
+}
