81531a38ade67c6bdfc58f0ecd37883dd90f2b7b
fResult <styxmaz@gmail.com>
2024-12-15 01:06:55 +0700

    Prepare BookingController.

--- src/main/java/dev/fResult/goutTogether/bookings/BookingController.java
+++ src/main/java/dev/fResult/goutTogether/bookings/BookingController.java
@@ -1,30 +1,66 @@
 package dev.fResult.goutTogether.bookings;
 
+import dev.fResult.goutTogether.bookings.dtos.BookingInfoResponse;
+import dev.fResult.goutTogether.bookings.dtos.BookingRequest;
+import dev.fResult.goutTogether.bookings.services.BookingService;
 import dev.fResult.goutTogether.helpers.ErrorHelper;
 import dev.fResult.goutTogether.tours.entities.Tour;
 import dev.fResult.goutTogether.tours.entities.TourCount;
 import dev.fResult.goutTogether.tours.repositories.TourCountRepository;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
+import org.hibernate.validator.constraints.UUID;
 import org.jobrunr.scheduling.BackgroundJob;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/bookings")
 public class BookingController {
   private final ErrorHelper errorHelper = new ErrorHelper(BookingController.class);
+
+  private final BookingService bookingService;
+
+  // Note: For Pessimistic Lock Testing Purpose
   private final TourCountRepository tourCountRepository;
 
-  public BookingController(TourCountRepository tourCountRepository) {
+  public BookingController(BookingService bookingService, TourCountRepository tourCountRepository) {
+    this.bookingService = bookingService;
     this.tourCountRepository = tourCountRepository;
   }
 
+  @PostMapping
+  public ResponseEntity<BookingInfoResponse> bookTour(
+      @UUID(message = "wrong format for headers `idempotent-key`") @RequestHeader("idempotent-key")
+          String idempotentKey,
+      @Validated @RequestBody BookingRequest body,
+      Authentication authentication) {
+
+    var bookedTour = bookingService.bookTour(idempotentKey, body);
+
+    return ResponseEntity.ok(bookedTour);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<BookingInfoResponse> cancelTourById(
+      @UUID(message = "wrong format for headers `idempotent-key`") @RequestHeader("idempotent-key")
+          String idempotentKey,
+      @NotNull @Min(1) Integer id,
+      @Validated @RequestBody BookingRequest body,
+      Authentication authentication) {
+
+    var cancelledTour = bookingService.cancelTour(idempotentKey, body);
+
+    return ResponseEntity.ok(cancelledTour);
+  }
+
+  // Note: For Pessimistic Lock Testing Purpose
   @PostMapping("/by-booking")
   public ResponseEntity<?> updateTourCountByBookingId(@RequestBody Payload body) {
     BackgroundJob.<SimpleService>schedule(
@@ -35,6 +71,7 @@ public class BookingController {
     return ResponseEntity.noContent().build();
   }
 
+  // Note: For Pessimistic Lock Testing Purpose
   @PostMapping("/by-tour")
   public ResponseEntity<?> updateTourCountByTourId(@RequestBody Payload body) {
     BackgroundJob.<SimpleService>schedule(
