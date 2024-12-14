5b2abf9f8987bebe298be8ddd31b1bf900bb63ab
fResult <styxmaz@gmail.com>
2024-12-15 01:42:37 +0700

    Create `QrCodeStatus` enum and add new field for `QrCodeReference` entity.

--- src/main/java/dev/fResult/goutTogether/qrcodes/QrCodeReference.java
+++ src/main/java/dev/fResult/goutTogether/qrcodes/QrCodeReference.java
@@ -1,12 +1,12 @@
 package dev.fResult.goutTogether.qrcodes;
 
-import dev.fResult.goutTogether.common.enumurations.QrStatus;
+import dev.fResult.goutTogether.common.enumurations.QrCodeStatus;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("qr_code_references")
-public record QrCodeReference(@Id Integer id, String content, QrStatus status) {
-  public static QrCodeReference of(Integer id, String content, QrStatus status) {
-    return new QrCodeReference(id, content, status);
+public record QrCodeReference(@Id Integer id, Integer bookingId, String content, QrCodeStatus status) {
+  public static QrCodeReference of(Integer id, Integer bookingId, String content, QrCodeStatus status) {
+    return new QrCodeReference(id, bookingId, content, status);
   }
 }
