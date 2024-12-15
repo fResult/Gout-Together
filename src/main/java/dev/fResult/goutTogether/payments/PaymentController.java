b86c746a4212eabce5ae54ff7e0fb52518b48239
fResult <styxmaz@gmail.com>
2024-12-15 15:28:54 +0700

    Update PaymentController and PaymentService.
    - Create BookingRepository interface
    - Update `PaymentService.generatePaymentQr` method signature
    - Implement `PaymentService.generatePaymentQr` method
    - Update `PaymentController` class

--- src/main/java/dev/fResult/goutTogether/payments/PaymentController.java
+++ src/main/java/dev/fResult/goutTogether/payments/PaymentController.java
@@ -2,10 +2,13 @@ package dev.fResult.goutTogether.payments;
 
 import static dev.fResult.goutTogether.common.Constants.API_PAYMENT_PATH;
 
+import com.google.zxing.WriterException;
 import dev.fResult.goutTogether.bookings.dtos.BookingInfoResponse;
 import dev.fResult.goutTogether.payments.services.PaymentService;
 import jakarta.validation.constraints.Min;
 import java.awt.image.BufferedImage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,22 +16,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(API_PAYMENT_PATH)
 public class PaymentController {
-    private final PaymentService paymentService;
+  private final Logger logger = LoggerFactory.getLogger(PaymentController.class);
 
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+  private final PaymentService paymentService;
 
-    @GetMapping(value = "/qr/{id}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<BufferedImage> getQrCodeImageById(@PathVariable @Min(1) Integer id) {
-        return ResponseEntity.ok(paymentService.generatePaymentQr(id));
-    }
-    
-    @PostMapping("/{bookingId}")
-    public ResponseEntity<BookingInfoResponse> payByBookingId(
-            @RequestHeader("idempotent-key") String idempotentKey,
-            @PathVariable @Min(1) Integer bookingId) {
+  public PaymentController(PaymentService paymentService) {
+    this.paymentService = paymentService;
+  }
 
-        return ResponseEntity.ok(paymentService.payByBookingId(bookingId, idempotentKey));
-    }
+  @GetMapping(value = "/qr/{qrCodeRefId}", produces = MediaType.IMAGE_PNG_VALUE)
+  public ResponseEntity<BufferedImage> getQrCodeImageById(@PathVariable @Min(1) Integer qrCodeRefId)
+      throws WriterException {
+
+    logger.debug("[getQrCodeImageById] Getting QR code image by id [{}]", qrCodeRefId);
+
+    return ResponseEntity.ok(paymentService.generatePaymentQr(qrCodeRefId));
+  }
+
+  @PostMapping("/{bookingId}")
+  public ResponseEntity<BookingInfoResponse> payByBookingId(
+      @RequestHeader("idempotent-key") String idempotentKey,
+      @PathVariable @Min(1) Integer bookingId) {
+
+    logger.debug("[payByBookingId] Paying by booking id [{}]", bookingId);
+
+    return ResponseEntity.ok(paymentService.payByBookingId(bookingId, idempotentKey));
+  }
 }
--- src/main/java/dev/fResult/goutTogether/payments/services/PaymentServiceImpl.java
+++ src/main/java/dev/fResult/goutTogether/payments/services/PaymentServiceImpl.java
@@ -1,13 +1,24 @@
 package dev.fResult.goutTogether.payments.services;
 
+import com.google.zxing.WriterException;
 import dev.fResult.goutTogether.bookings.dtos.BookingInfoResponse;
-
+import dev.fResult.goutTogether.qrcodes.QrCodeService;
 import java.awt.image.BufferedImage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PaymentServiceImpl implements PaymentService {
+  private final Logger logger = LoggerFactory.getLogger(PaymentServiceImpl.class);
+
+  private final QrCodeService qrCodeService;
+
+  public PaymentServiceImpl(QrCodeService qrCodeService) {
+    this.qrCodeService = qrCodeService;
+  }
+
   @Override
-  public BufferedImage generatePaymentQr(Integer id) {
-    throw new UnsupportedOperationException("Not Implement Yet");
+  public BufferedImage generatePaymentQr(int id) throws WriterException {
+    return qrCodeService.generateQrCodeImageById(id);
   }
 
   @Override
