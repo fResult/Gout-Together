fa65c01af8819140d70c2bc8fdbf232b0a9b9126
fResult <styxmaz@gmail.com>
2024-12-15 17:10:42 +0700

    Create `TourCompanyWalletRepository.findOneByTourCompanyId` method.

--- src/main/java/dev/fResult/goutTogether/wallets/repositories/TourCompanyWalletRepository.java
+++ src/main/java/dev/fResult/goutTogether/wallets/repositories/TourCompanyWalletRepository.java
@@ -1,8 +1,14 @@
 package dev.fResult.goutTogether.wallets.repositories;
 
+import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
 import dev.fResult.goutTogether.wallets.entities.TourCompanyWallet;
+import java.util.Optional;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TourCompanyWalletRepository extends CrudRepository<TourCompanyWallet, Integer> {}
+public interface TourCompanyWalletRepository extends CrudRepository<TourCompanyWallet, Integer> {
+  Optional<TourCompanyWallet> findOneByTourCompanyId(
+      AggregateReference<TourCompany, Integer> tourCompanyId);
+}
