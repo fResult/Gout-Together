40c1331c88fcd274cff58b64035060a71fdae546
fResult <styxmaz@gmail.com>
2024-12-18 15:13:40 +0700

    Implement `TourCountService.incrementTourCount` method.

--- src/main/java/dev/fResult/goutTogether/tours/services/TourCountService.java
+++ src/main/java/dev/fResult/goutTogether/tours/services/TourCountService.java
@@ -1,14 +1,17 @@
 package dev.fResult.goutTogether.tours.services;
 
+import dev.fResult.goutTogether.helpers.ErrorHelper;
 import dev.fResult.goutTogether.tours.entities.TourCount;
 import dev.fResult.goutTogether.tours.repositories.TourCountRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TourCountService {
   private final Logger logger = LoggerFactory.getLogger(TourCountService.class);
+  private final ErrorHelper errorHelper = new ErrorHelper(TourCountService.class);
 
   private final TourCountRepository tourCountRepository;
 
@@ -25,7 +28,25 @@ public class TourCountService {
     return createdTourCount;
   }
 
-  public void incrementTourCount() {
-    // Increment the tour count
+  public void incrementTourCount(int tourId) {
+    logger.debug(
+        "[incrementTourCount] Incrementing {} with tourId [{}]",
+        TourCount.class.getSimpleName(),
+        tourId);
+
+    var tourCount =
+        tourCountRepository
+            .findOneByTourId(AggregateReference.to(tourId))
+            .orElseThrow(
+                errorHelper.entityWithSubResourceNotFound(
+                    "incrementTourCount", TourCount.class, "tourId", String.valueOf(tourId)));
+
+    var tourCountToIncrement = tourCount.increaseAmount(1);
+    tourCountRepository.save(tourCountToIncrement);
+
+    logger.info(
+        "[incrementTourCount] Incremented {} to {}",
+        TourCount.class.getSimpleName(),
+        tourCount.amount());
   }
 }
