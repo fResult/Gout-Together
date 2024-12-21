57761bdafa39b5144860453f82d733cc70af2c0f
fResult <styxmaz@gmail.com>
2024-12-22 03:25:55 +0700

    Create `whenGetConsumerAndCompanyWalletsThenSuccess` test case.

--- src/test/java/dev/fResult/goutTogether/wallets/WalletServiceTest.java
+++ src/test/java/dev/fResult/goutTogether/wallets/WalletServiceTest.java
@@ -4,7 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import dev.fResult.goutTogether.bookings.entities.Booking;
+import dev.fResult.goutTogether.common.enumurations.BookingStatus;
+import dev.fResult.goutTogether.common.enumurations.TourStatus;
 import dev.fResult.goutTogether.common.exceptions.EntityNotFoundException;
+import dev.fResult.goutTogether.common.utils.UUIDV7;
+import dev.fResult.goutTogether.tourCompanies.entities.TourCompany;
+import dev.fResult.goutTogether.tours.entities.Tour;
+import dev.fResult.goutTogether.tours.services.TourService;
 import dev.fResult.goutTogether.users.entities.User;
 import dev.fResult.goutTogether.wallets.dtos.UserWalletInfoResponse;
 import dev.fResult.goutTogether.wallets.entities.TourCompanyWallet;
@@ -14,6 +21,7 @@ import dev.fResult.goutTogether.wallets.repositories.UserWalletRepository;
 import dev.fResult.goutTogether.wallets.services.WalletServiceImpl;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,19 +33,22 @@ import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
 @ExtendWith(MockitoExtension.class)
 class WalletServiceTest {
-  private static final int WALLET_ID = 1;
+  private static final int USER_WALLET_ID = 1;
+  private static final String IDEMPOTENCY_KEY = UUIDV7.randomUUID().toString();
 
   @InjectMocks private WalletServiceImpl walletService;
 
   @Mock UserWalletRepository userWalletRepository;
   @Mock TourCompanyWalletRepository tourCompanyWalletRepository;
+  @Mock TourService tourService;
 
   @Test
   void whenCreateConsumerWalletThenSuccess() {
     // Arrange
     var USER_ID = 1;
     var mockCreatedUserWallet =
-        UserWallet.of(WALLET_ID, AggregateReference.to(USER_ID), Instant.now(), BigDecimal.ZERO);
+        UserWallet.of(
+            USER_WALLET_ID, AggregateReference.to(USER_ID), Instant.now(), BigDecimal.ZERO);
     when(userWalletRepository.save(any(UserWallet.class))).thenReturn(mockCreatedUserWallet);
 
     // Actual
@@ -52,8 +63,9 @@ class WalletServiceTest {
     // Arrange
     var USER_ID = 1;
     var userRef = AggregateReference.<User, Integer>to(USER_ID);
-    var mockUserWallet = UserWallet.of(WALLET_ID, userRef, Instant.now(), BigDecimal.ZERO);
-    var expectedFoundUserWallet = UserWalletInfoResponse.of(WALLET_ID, USER_ID, BigDecimal.ZERO);
+    var mockUserWallet = UserWallet.of(USER_WALLET_ID, userRef, Instant.now(), BigDecimal.ZERO);
+    var expectedFoundUserWallet =
+        UserWalletInfoResponse.of(USER_WALLET_ID, USER_ID, BigDecimal.ZERO);
 
     when(userWalletRepository.findOneByUserId(userRef)).thenReturn(Optional.of(mockUserWallet));
 
@@ -69,7 +81,8 @@ class WalletServiceTest {
     // Arrange
     var NOT_FOUND_USER_ID = 99999;
     var expectedErrorMessage =
-        String.format("%s id [%s] not found", UserWallet.class.getSimpleName(), NOT_FOUND_USER_ID);
+        String.format(
+            "%s with userId [%s] not found", UserWallet.class.getSimpleName(), NOT_FOUND_USER_ID);
     var notFoundUserRef = AggregateReference.<User, Integer>to(NOT_FOUND_USER_ID);
     when(userWalletRepository.findOneByUserId(notFoundUserRef)).thenReturn(Optional.empty());
 
@@ -86,12 +99,12 @@ class WalletServiceTest {
     // Arrange
     var USER_ID = 1;
     var userRef = AggregateReference.<User, Integer>to(USER_ID);
-    var walletToDelete = UserWallet.of(WALLET_ID, userRef, Instant.now(), BigDecimal.ZERO);
+    var walletToDelete = UserWallet.of(USER_WALLET_ID, userRef, Instant.now(), BigDecimal.ZERO);
     when(userWalletRepository.findOneByUserId(userRef)).thenReturn(Optional.of(walletToDelete));
     doNothing().when(userWalletRepository).delete(any(UserWallet.class));
 
     // Actual
-    var actualIsSuccess = walletService.deleteConsumerWalletByUserId(WALLET_ID);
+    var actualIsSuccess = walletService.deleteConsumerWalletByUserId(USER_WALLET_ID);
 
     // Assert
     verify(userWalletRepository, times(1)).delete(walletToDelete);
@@ -104,7 +117,7 @@ class WalletServiceTest {
     var COMPANY_ID = 1;
     var mockCreatedCompanyWallet =
         TourCompanyWallet.of(
-            WALLET_ID, AggregateReference.to(COMPANY_ID), Instant.now(), BigDecimal.ZERO);
+            USER_WALLET_ID, AggregateReference.to(COMPANY_ID), Instant.now(), BigDecimal.ZERO);
     when(tourCompanyWalletRepository.save(any(TourCompanyWallet.class)))
         .thenReturn(mockCreatedCompanyWallet);
 
@@ -113,5 +126,48 @@ class WalletServiceTest {
 
     // Assert
     assertEquals(mockCreatedCompanyWallet, actualCreatedWallet);
+  }
+
+  @Test
+  void whenGetConsumerAndCompanyWalletsThenSuccess() {
+    // Arrange
+    var COMPANY_WALLET_ID = 12;
+    var userRef = AggregateReference.<User, Integer>to(1);
+    var tourRef = AggregateReference.<Tour, Integer>to(1);
+    var tourCompanyRef = AggregateReference.<TourCompany, Integer>to(1);
+    var mockBooking =
+        Booking.of(
+            1,
+            userRef,
+            tourRef,
+            BookingStatus.PENDING.name(),
+            Instant.now(),
+            Instant.now(),
+            IDEMPOTENCY_KEY);
+    var mockTour =
+        Tour.of(
+            1,
+            tourCompanyRef,
+            "",
+            "Camping in Bangkok",
+            "Bangkok, Thailand",
+            10,
+            Instant.now().plus(45, ChronoUnit.DAYS),
+            TourStatus.APPROVED.name());
+    var mockUserWallet = UserWallet.of(USER_WALLET_ID, userRef, Instant.now(), BigDecimal.ZERO);
+    var mockTourCompanyWallet =
+        TourCompanyWallet.of(COMPANY_WALLET_ID, tourCompanyRef, Instant.now(), BigDecimal.ZERO);
+
+    when(userWalletRepository.findOneByUserId(userRef)).thenReturn(Optional.of(mockUserWallet));
+    when(tourService.getTourById(anyInt())).thenReturn(mockTour);
+    when(tourCompanyWalletRepository.findOneByTourCompanyId(tourCompanyRef))
+        .thenReturn(Optional.of(mockTourCompanyWallet));
+
+    // Actual
+    var actualUserWallet = walletService.getConsumerAndTourCompanyWallets(mockBooking);
+
+    // Assert
+    assertEquals(mockUserWallet, actualUserWallet.getFirst());
+    assertEquals(mockTourCompanyWallet, actualUserWallet.getSecond());
   }
 }
