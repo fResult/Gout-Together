6fb530169690116bc2632a8b5aae14cdea5158fb
fResult <styxmaz@gmail.com>
2024-12-22 22:53:07 +0700

    Create `whenTopUpUserWalletThenSuccess` test case.

--- src/test/java/dev/fResult/goutTogether/wallets/WalletControllerTest.java
+++ src/test/java/dev/fResult/goutTogether/wallets/WalletControllerTest.java
@@ -5,11 +5,15 @@ import static dev.fResult.goutTogether.common.Constants.ROLES_CLAIM;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.fResult.goutTogether.common.enumurations.UserRoleName;
+import dev.fResult.goutTogether.common.utils.UUIDV7;
 import dev.fResult.goutTogether.wallets.dtos.UserWalletInfoResponse;
+import dev.fResult.goutTogether.wallets.dtos.WalletTopUpRequest;
 import dev.fResult.goutTogether.wallets.entities.UserWallet;
 import dev.fResult.goutTogether.wallets.services.WalletService;
 import java.math.BigDecimal;
@@ -20,6 +24,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -43,18 +49,21 @@ class WalletControllerTest {
     mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
   }
 
+  private Authentication buildAuthentication(int userId, UserRoleName roleName) {
+    var jwt =
+        Jwt.withTokenValue("token")
+            .header("alg", "none")
+            .claim(RESOURCE_ID_CLAIM, String.valueOf(userId))
+            .claim(ROLES_CLAIM, List.of("ROLE_" + roleName.name()))
+            .build();
+    return new JwtAuthenticationToken(jwt);
+  }
+
   @Test
   void whenGetMyUserWalletThenSuccess() throws Exception {
     // Arrange
     var USER_ID = 1;
-    var jwt =
-        Jwt.withTokenValue("token")
-            .header("alg", "none")
-            .claim(RESOURCE_ID_CLAIM, String.valueOf(USER_ID))
-            .claim(ROLES_CLAIM, List.of("ROLE_CONSUMER"))
-            .build();
-    var authentication = new JwtAuthenticationToken(jwt);
-
+    var authentication = buildAuthentication(USER_ID, UserRoleName.CONSUMER);
     var userWallet =
         UserWallet.of(1, AggregateReference.to(USER_ID), Instant.now(), BigDecimal.TEN);
     var expectedUserWalletInfo =
@@ -65,8 +74,35 @@ class WalletControllerTest {
     var resultActions = mockMvc.perform(get(WALLET_API + "/me").principal(authentication));
 
     // Assert
+    resultActions.andExpect(status().isOk()).andExpect(jsonPath("$.userId").value(USER_ID));
+  }
+
+  @Test
+  void whenTopUpUserWalletThenSuccess() throws Exception {
+    // Arrange
+    var USER_ID = 1;
+    var AMOUNT_TO_TOP_UP = BigDecimal.valueOf(300);
+    var IDEMPOTENT_KEY = UUIDV7.randomUUID().toString();
+    var authentication = buildAuthentication(USER_ID, UserRoleName.CONSUMER);
+    var body = WalletTopUpRequest.of(AMOUNT_TO_TOP_UP);
+    var expectedBalanceAfterTopUp = AMOUNT_TO_TOP_UP.add(BigDecimal.valueOf(100));
+    var expectedUserWalletInfo = UserWalletInfoResponse.of(1, USER_ID, expectedBalanceAfterTopUp);
+    when(walletService.topUpConsumerWallet(USER_ID, IDEMPOTENT_KEY, body))
+        .thenReturn(expectedUserWalletInfo);
+
+    // Actual
+    var resultActions =
+        mockMvc.perform(
+            post(WALLET_API + "/top-up")
+                .principal(authentication)
+                .header("idempotent-key", IDEMPOTENT_KEY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(body)));
+
+    // Assert
     resultActions
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.userId").value(USER_ID));
+        .andExpect(jsonPath("$.userId").value(USER_ID))
+        .andExpect(jsonPath("$.balance").value(expectedBalanceAfterTopUp));
   }
 }
