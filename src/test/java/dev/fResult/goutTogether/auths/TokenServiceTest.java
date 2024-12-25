60dbaca08354afbd406ceea73d851c4197d2f037
fResult <styxmaz@gmail.com>
2024-12-26 00:10:27 +0700

    Create `IssueAccessToken.byAuthenticatedUserThenSuccess` test case.

--- src/test/java/dev/fResult/goutTogether/auths/TokenServiceTest.java
+++ src/test/java/dev/fResult/goutTogether/auths/TokenServiceTest.java
@@ -2,20 +2,22 @@ package dev.fResult.goutTogether.auths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 import dev.fResult.goutTogether.auths.dtos.AuthenticatedUser;
+import dev.fResult.goutTogether.auths.entities.UserLogin;
 import dev.fResult.goutTogether.auths.repositories.RefreshTokenRepository;
 import dev.fResult.goutTogether.auths.services.CustomUserDetailsService;
 import dev.fResult.goutTogether.auths.services.TokenService;
 import dev.fResult.goutTogether.common.enumurations.UserRoleName;
 import java.time.Instant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 
@@ -28,27 +30,35 @@ class TokenServiceTest {
   @Mock private CustomUserDetailsService userDetailsService;
   @Mock private JwtEncoder jwtEncoder;
 
-  @Spy @InjectMocks
-  private TokenService tokenService =
-      new TokenService(
-          refreshTokenRepository,
-          userDetailsService,
-          accessTokenExpiredInSeconds,
-          refreshTokenExpiredInSeconds,
-          jwtEncoder);
+  private TokenService tokenService;
 
-  @Test
-  void whenIssueAccessTokenWithAuthenticatedUserThenSuccess() {
-    // Arrange
-    var authenticatedUser =
-        AuthenticatedUser.of(1, "email@example.com", "P@$$w0rd", UserRoleName.ADMIN);
-    var mockIssuedToken = "token";
-    doReturn(mockIssuedToken).when(tokenService).encodeClaimToJwt(any(JwtClaimsSet.class));
+  @BeforeEach
+  void setUp() {
+    tokenService =
+        spy(
+            new TokenService(
+                refreshTokenRepository,
+                userDetailsService,
+                accessTokenExpiredInSeconds,
+                refreshTokenExpiredInSeconds,
+                jwtEncoder));
+  }
 
-    // Actual
-    var actualIssuedAccessToken = tokenService.issueAccessToken(authenticatedUser, Instant.now());
+  @Nested
+  class IssueAccessToken {
+    @Test
+    void byAuthenticatedUserThenSuccess() {
+      // Arrange
+      var authenticatedUser =
+          AuthenticatedUser.of(1, "email@example.com", "P@$$w0rd", UserRoleName.ADMIN);
+      var mockIssuedAccessToken = "token";
+      doReturn(mockIssuedAccessToken).when(tokenService).encodeClaimToJwt(any(JwtClaimsSet.class));
 
-    // Assert
-    assertEquals(mockIssuedToken, actualIssuedAccessToken);
+      // Actual
+      var actualIssuedAccessToken = tokenService.issueAccessToken(authenticatedUser, Instant.now());
+
+      // Assert
+      assertEquals(mockIssuedAccessToken, actualIssuedAccessToken);
+    }
   }
 }
