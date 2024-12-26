fe9470a3623f07b0d101f4afe31825529cf521a5
fResult <styxmaz@gmail.com>
2024-12-26 15:48:47 +0700

    Create `QrCodeHelperTest.whenGenerateQrCode_ThenSuccess` test case.

---
+++ src/test/java/dev/fResult/goutTogether/common/helpers/QrCodeHelperTest.java
@@ -0,0 +1,35 @@
+package dev.fResult.goutTogether.common.helpers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import java.awt.image.BufferedImage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class QrCodeHelperTest {
+  @Test
+  void whenGenerateQrCode_ThenSuccess() throws WriterException {
+    try (var mockedMatrixToImageWriter = mockStatic(MatrixToImageWriter.class)) {
+      // Arrange
+      var barcodeTextInput = "Hello, World!";
+      var expectedQrCodeImage = new BufferedImage(300, 300, BufferedImage.TYPE_INT_RGB);
+
+      mockedMatrixToImageWriter
+          .when(() -> MatrixToImageWriter.toBufferedImage(any(BitMatrix.class)))
+          .thenReturn(expectedQrCodeImage);
+
+      // Actual
+      var actualQrCodeImage = QrCodeHelper.generateQrCodeImage(barcodeTextInput);
+
+      // Assert
+      assertEquals(expectedQrCodeImage, actualQrCodeImage);
+    }
+  }
+}
