6ad30de2e899a90d9ed96f1735ad3eba6d667914
fResult <styxmaz@gmail.com>
2025-01-06 18:55:23 +0700

    Upgrade gradle version, and spring boot version using \`libs.version.toml\` file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 import io.github.cdimascio.dotenv.Dotenv
+import io.github.cdimascio.dotenv.DotenvEntry
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
+
+plugins {
+  application
+  java
+  id("org.springframework.boot") version libs.versions.springboot.get()
+  id("io.spring.dependency-management") version "1.1.7"
+}
 
 buildscript {
   repositories {
@@ -9,13 +17,6 @@ buildscript {
   dependencies {
     classpath("io.github.cdimascio:java-dotenv:5.2.2")
   }
-}
-
-plugins {
-  application
-  java
-  id("org.springframework.boot") version "3.4.0"
-  id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "dev.fResult"
@@ -37,24 +38,30 @@ dependencies {
   implementation("io.opentelemetry:opentelemetry-api")
   agent("io.opentelemetry.javaagent:opentelemetry-javaagent:2.9.0")
 
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  developmentOnly(libs.spring.boot.devtools)
 
-  implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-validation")
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+  implementation(libs.spring.boot.starter.actuator)
+  implementation(libs.spring.boot.starter.data.jdbc)
+  implementation(libs.spring.boot.starter.oauth2.resource.server)
+  implementation(libs.spring.boot.starter.security)
+  implementation(libs.spring.boot.starter.validation)
+  implementation(libs.spring.boot.starter.web)
+  runtimeOnly("io.micrometer:micrometer-registry-prometheus")
   implementation("org.bouncycastle:bcprov-jdk18on:1.79") // As an Argon2PasswordEncoder's dependency in Spring Security
   implementation("org.flywaydb:flyway-core")
   implementation("org.flywaydb:flyway-database-postgresql")
   implementation("io.github.cdimascio:java-dotenv:5.2.2")
+  implementation("commons-validator:commons-validator:1.9.0")
+  implementation("org.jobrunr:jobrunr-spring-boot-3-starter:7.3.2")
+  implementation("com.google.zxing:core:3.5.3")
+  implementation("com.google.zxing:javase:3.5.3")
 
   runtimeOnly("io.micrometer:micrometer-registry-prometheus")
   runtimeOnly("org.postgresql:postgresql")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation(libs.spring.boot.starter.test)
+  testImplementation(libs.spring.boot.testcontainers)
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.springframework.boot:spring-boot-testcontainers")
   testImplementation("org.testcontainers:junit-jupiter")
   testImplementation("org.testcontainers:postgresql")
 
@@ -85,8 +92,6 @@ tasks.withType<BootRun> {
   doFirst {
     jvmArgs(listOf("-javaagent:build/agent/opentelemetry-javaagent.jar"))
     val dotenv = Dotenv.configure().load()
-    dotenv.entries().forEach { entry ->
-      environment(entry.key, entry.value)
-    }
+    dotenv.entries().forEach { entry -> environment(entry.key, entry.value) }
   }
 }
